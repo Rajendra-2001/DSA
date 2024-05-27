53e7c26a91b5fad2ed6ca30184dafc92f0682718
Rajendra-2001 <rajendrakishore84@gmail.com>
2024-05-27 22:01:03 +0530

    Optimised

--- DSA/Binary_Search/1608_Special_Array_With_X_Elements_Greater_Than_or_Equal_X.cpp
+++ DSA/Binary_Search/1608_Special_Array_With_X_Elements_Greater_Than_or_Equal_X.cpp
@@ -1,6 +1,7 @@
 class Solution
 {
 public:
+  // SC: O(n)
   int count(vector<int> &nums, int target)
   {
     int ans = 0;
@@ -11,6 +12,7 @@ public:
     }
     return ans;
   }
+
   int specialArray(vector<int> &nums)
   {
     sort(nums.begin(), nums.end());
@@ -31,6 +33,28 @@ public:
         end = mid - 1;
     }
 
+    return -1;
+  }
+
+  // SC: O(nlog(n))
+
+  int specialArray(vector<int> &nums)
+  {
+    sort(nums.begin(), nums.end());
+    int n = nums.size();
+    int l = 0, r = n;
+    while (l <= r)
+    {
+      int mid = l + (r - l) / 2;
+      int idx = lower_bound(nums.begin(), nums.end(), mid) - nums.begin();
+      int count = n - idx;
+      if (count == mid)
+        return mid;
+      else if (count > mid)
+        l = mid + 1;
+      else
+        r = mid - 1;
+    }
     return -1;
   }
 };
