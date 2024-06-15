2530cdeae6f7c0583fbd979a10235cf3ffa20c2b
Rajendra-2001 <rajendrakishore84@gmail.com>
2024-06-15 22:47:24 +0530

    Day 20

--- DSA/DP/322_Coin_Change.cpp
+++ DSA/DP/322_Coin_Change.cpp
@@ -32,6 +32,41 @@ int coinChange(vector<int> &coins, int amount)
     return -1; // If no solution found, return -1
   return ans;
 }
+// Tabulation
+int coinChange(vector<int> &coins, int amount)
+{
+  int n = coins.size();
+  vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+
+  // Initialize the base case for the first row
+  for (int tar = 0; tar <= amount; tar++)
+  {
+    if (tar % coins[0] == 0)
+      dp[0][tar] = tar / coins[0];
+    else
+      dp[0][tar] = 1e9; // Use a large value to signify no solution
+  }
+
+  // Fill the DP table
+  for (int ind = 1; ind < n; ind++)
+  {
+    for (int tar = 0; tar <= amount; tar++)
+    {
+      int notTake = dp[ind - 1][tar]; // Not taking the current coin
+      int take = 1e9;                 // Default to large value (no solution)
+      if (coins[ind] <= tar)
+      {
+        take = 1 + dp[ind][tar - coins[ind]]; // Take the current coin
+      }
+      dp[ind][tar] = min(take, notTake); // Minimum of taking or not taking
+    }
+  }
+
+  int ans = dp[n - 1][amount];
+  if (ans >= 1e9)
+    return -1; // If the solution is not found, return -1
+  return ans;
+}
 int main()
 {
   vector<int> coins = {1, 2, 5};
